03ba7adc8458e8e8b2b9664c472bf037c79c4ff2
chaduvulaprasanth <chaduvulaprasanth1998@gmail.com>
2019-09-05 20:44:51 +0530

    passing function as parameter and returns the same function

--- main.js
+++ main.js
@@ -37,6 +37,36 @@ paraf(function (){return 3});
 paraf(function (){return 3}());
 // output is 5
 
+// writing above whole function in other format
+function cbf (){
+    return 3;
+    }
+    
+    function paraf(cbf){
+        return cbf + 2;
+    }
+    paraf(cbf())
+ // output is 5
 
+
+// passing a function as parameter and returning function * function
+function cbf (){
+    return 3;
+    }
+    
+    function paraf(cbf){
+        return cbf + cbf;
+    }
+    paraf(cbf)
+    // output is 
+    // "function cbf (){
+    //     return 3;
+    //     }function cbf (){
+    //     return 3;
+    //     }"
+
+    paraf(cbf())
+    // output is 6
+    
 
 
